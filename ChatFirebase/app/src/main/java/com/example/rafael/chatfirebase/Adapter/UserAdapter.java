cad1fa07d5f33b28e3b19f532d64b16ad78b9be7
RafaMateos <rafaelmateostorres@gmail.com>
2019-01-07 00:09:57 +0100

    Display all user register in application to chat with him

---
+++ ChatFirebase/app/src/main/java/com/example/rafael/chatfirebase/Adapter/UserAdapter.java
@@ -0,0 +1,74 @@
+package com.example.rafael.chatfirebase.Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.rafael.chatfirebase.Model.User;
+import com.example.rafael.chatfirebase.R;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class UserAdapter  extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<User> mUsers;
+
+    public UserAdapter(Context mContext,List<User> mUsers){
+
+        this.mContext = mContext;
+        this.mUsers = mUsers;
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item,viewGroup,false);
+        return new UserAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+        User user = mUsers.get(i);
+        viewHolder.username.setText(user.getUsername());
+        if(user.getImageUrl().equals("default")){
+
+            viewHolder.profile_image.setImageResource(R.mipmap.ic_launcher);
+
+        }else{
+
+            Glide.with(mContext).load(user.getImageUrl()).into(viewHolder.profile_image);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUsers.size();
+    }
+
+    public class ViewHolder extends  RecyclerView.ViewHolder{
+
+        public TextView username;
+        public ImageView profile_image;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            username = itemView.findViewById(R.id.username);
+            profile_image = itemView.findViewById(R.id.profile_image);
+
+        }
+    }
+}
